4a1deba62a12111934338fed1e8679f2be6450b7
lomic28 <try7loving@gmail.com>
2018-01-13 00:29:08 +0800

    remove: move lodash file to repo:extras-score

--- lodash/Array/flattenDeep.js
+++
@@ -1,9 +0,0 @@
-function flattenDeep(arr) {
-    return arr.reduce((previousVlaue, currentValue) => {
-        if (Array.isArray(currentValue)) {
-            return previousVlaue.concat(flattenDeep(currentValue));
-        } else {
-            return previousVlaue.push(currentValue);
-        }
-    }, []);
-}
